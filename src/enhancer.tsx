11253ed6c8401bcfa49819d62d9ead224e458608
aleexwae4-lab <aleexwae4@gmail.com>
2026-09-01 06:02:55 -0600

    feat: mount companies experience layer

--- src/enhancer.tsx
+++ src/enhancer.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import DepartmentExperienceLayer from './DepartmentExperienceLayer'
+import CompaniesPortalLayer from './CompaniesPortalLayer'
 
 const host=document.createElement('div')
 host.id='wae-department-experience-root'
 document.body.appendChild(host)
-ReactDOM.createRoot(host).render(<React.StrictMode><DepartmentExperienceLayer/></React.StrictMode>)
+ReactDOM.createRoot(host).render(
+  <React.StrictMode>
+    <DepartmentExperienceLayer/>
+    <CompaniesPortalLayer/>
+  </React.StrictMode>
+)
